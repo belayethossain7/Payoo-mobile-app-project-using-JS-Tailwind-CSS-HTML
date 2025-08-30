12c620fa98a6d34e9e2acd745ecd860d1a91cdb0
Md. Belayet Hossain <belayet.hossain6847@gmail.com>
2025-08-30 21:48:18 +0600

    add event listener

---
+++ js/script.js
@@ -0,0 +1,7 @@
+//log in button functionality
+
+document.getElementById('btn-login').addEventListener('click', function (e){
+    e.preventDefault()
+    console.log('log btn clicked');
+    console.log(e);
+})
