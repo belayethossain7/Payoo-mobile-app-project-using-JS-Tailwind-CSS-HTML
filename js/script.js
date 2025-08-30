66c26a7ec03f8b478476ffa2b350a56817fbe2ca
Md. Belayet Hossain <belayet.hossain6847@gmail.com>
2025-08-30 22:08:41 +0600

    added login functionality and redirect to home

--- js/script.js
+++ js/script.js
@@ -2,6 +2,24 @@
 
 document.getElementById('btn-login').addEventListener('click', function (e){
     e.preventDefault()
-    console.log('log btn clicked');
-    console.log(e);
+    
+    const mobileNumber = 123456789;
+    const pinNumber = 1234;
+//mobile value
+    const MobileNumValue = document.getElementById('mobile-number').value ;
+
+    const mNConvert = parseInt(MobileNumValue);
+
+//pin value
+    const pinNumValue = document.getElementById('pin-number').value ;
+
+    const pinConvert = parseInt(pinNumValue);
+
+    if(mobileNumber===mNConvert && pinNumber===pinConvert){
+        window.location.href="./home.html";
+    }
+
+    else{
+        alert("Invalid Input. Input Your mob. and pass. carefully")
+    }
 })
